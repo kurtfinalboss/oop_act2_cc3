1fe118a0dac3e82fc9152693bc4ab831680cc7f0
kurtpogi <kurtvirtudez011407@gmail.com>
2026-02-02 21:06:54 +0800

    Bus-code

--- Bus/src/main/java/bus/bus/Bus.java
+++ Bus/src/main/java/bus/bus/Bus.java
@@ -9,8 +9,32 @@ package bus.bus;
  * @author paulh
  */
 public class Bus {
+  
+    public Driver driver;
+    public Engine engine;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    Bus(Driver driver){
+        this.driver = driver;
+        this.engine = new Engine("Diesel");
+    }
+
+    public String engineIdentity(){
+        return engine.identity();
+    }
+
+    public static void main(String[] args){
+
+        Driver driver = new Driver(10);
+
+        Passenger passenger = new Passenger("Student");
+
+        Bus bus = new Bus(driver);
+
+        String output =
+                driver.identity() + "\n" +
+                passenger.identity() + "\n" +
+                bus.engine.identity(); 
+
+        System.out.println(output);
     }
 }
